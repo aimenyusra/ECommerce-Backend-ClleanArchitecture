5f13eee32dd5fd6dd6351a3ed770ea3b69b8ddd7
aimenyusra <aimenbather@gmail.com>
2026-08-10 18:13:28 +0530

    feat(application):implemented delete product command and handler

---
+++ src/ECommerce.Application/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,5 @@
+﻿using ECommerce.Application.Common.Messaging;
+
+namespace ECommerce.Application.Products.Commands.DeleteProduct;
+
+public sealed record DeleteProductCommand(int Id) : ICommand;
---
+++ src/ECommerce.Application/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Application.Common.Interfaces;
+using ECommerce.Application.Common.Messaging;
+using ECommerce.Domain.Exceptions;
+
+namespace ECommerce.Application.Products.Commands.DeleteProduct;
+
+public sealed class DeleteProductCommandHandler : ICommandHandler<DeleteProductCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteProductCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _unitOfWork.Products.GetByIdAsync(request.Id, cancellationToken);
+        if (product is null)
+            throw new ProductNotFoundException(request.Id);
+
+        _unitOfWork.Products.Remove(product);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
