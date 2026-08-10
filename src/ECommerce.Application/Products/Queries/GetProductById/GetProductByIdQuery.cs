435850fd29a6f44145a7f5e64ac7081ec07bfc04
aimenyusra <aimenbather@gmail.com>
2026-08-10 17:06:36 +0530

    feat(application):implemented Product query and query handler

---
+++ src/ECommerce.Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Application.Common.Messaging;
+using ECommerce.Application.Products.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Products.Queries.GetProductById
+{
+    public sealed record GetProductByIdQuery(int Id) : IQuery<ProductDto>;
+}
---
+++ src/ECommerce.Application/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ECommerce.Application.Common.Interfaces;
+using ECommerce.Application.Common.Messaging;
+using ECommerce.Application.Products.DTOs;
+using ECommerce.Domain.Exceptions;
+
+namespace ECommerce.Application.Products.Queries.GetProductById;
+
+public sealed class GetProductByIdQueryHandler : IQueryHandler<GetProductByIdQuery, ProductDto>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetProductByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<ProductDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _unitOfWork.Products.GetByIdAsync(request.Id, cancellationToken);
+        if (product is null)
+            throw new ProductNotFoundException(request.Id);
+
+        return _mapper.Map<ProductDto>(product);
+    }
+}
