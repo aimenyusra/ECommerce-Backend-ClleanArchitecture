4f2386b3d45ac110b3295f7392a0ecc50000b9ed
aimenyusra <aimenbather@gmail.com>
2026-08-11 21:52:15 +0530

    feat (application):added get category queries

---
+++ src/ECommerce.Application/Categories/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,6 @@
+﻿using ECommerce.Application.Categories.DTOs;
+using ECommerce.Application.Common.Messaging;
+
+namespace ECommerce.Application.Categories.Queries.GetCategories;
+
+public sealed record GetCategoriesQuery : IQuery<IReadOnlyList<CategoryDto>>;
---
+++ src/ECommerce.Application/Categories/Queries/GetCategories/GetCategoriesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ECommerce.Application.Categories.DTOs;
+using ECommerce.Application.Common.Interfaces;
+using ECommerce.Application.Common.Messaging;
+
+namespace ECommerce.Application.Categories.Queries.GetCategories;
+
+public sealed class GetCategoriesQueryHandler : IQueryHandler<GetCategoriesQuery, IReadOnlyList<CategoryDto>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetCategoriesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<CategoryDto>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await _unitOfWork.Categories.GetAllAsync(cancellationToken);
+        return _mapper.Map<IReadOnlyList<CategoryDto>>(categories);
+    }
+}
