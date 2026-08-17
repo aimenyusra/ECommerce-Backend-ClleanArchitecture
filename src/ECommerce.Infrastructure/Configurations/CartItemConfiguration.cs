eb0f89fd220bde31b7938a5e88024693502863cc
aimenyusra <aimenbather@gmail.com>
2026-08-17 20:25:10 +0530

    feat(infrastructure):intial foler/file setup

---
+++ src/ECommerce.Infrastructure/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Infrastructure.Configurations
+{
+    internal class CartItemConfiguration
+    {
+    }
+}
