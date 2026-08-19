f4820435a396cc439f0a73b3e916be0ef87a6e49
aimenyusra <aimenbather@gmail.com>
2026-08-19 18:07:57 +0530

    feat(infrastructure):added ApplicationDbContext

--- src/ECommerce.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/ECommerce.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reflection;
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace ECommerce.Infrastructure.Persistence
+namespace ECommerce.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
 {
-    internal class ApplicationDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<Cart> Carts => Set<Cart>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
     }
 }
